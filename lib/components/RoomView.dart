278d3f70fa13efd8a476599856fe476c5343f2db
DaichiK0919 <new.ihadaacidkit@gmail.com>
2024-10-06 17:46:38 +0900

    feature/SCRUM-36 merge失敗を修正

--- lib/components/RoomView.dart
+++ lib/components/RoomView.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bias_profile/constants.dart';
+import 'package:bias_profile/commons/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:bias_profile/components/components.dart';
