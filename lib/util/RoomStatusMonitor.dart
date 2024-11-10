2066cb0e146636f813923ad4a47c456bdbf35331
DaichiK0919 <new.ihadaacidkit@gmail.com>
2024-11-11 03:03:01 +0900

    RoomStatusMonitor をswitchでの分岐にリファクタリング

--- lib/util/RoomStatusMonitor.dart
+++ lib/util/RoomStatusMonitor.dart
@@ -4,7 +4,6 @@ import 'package:bias_profile/components/components.dart';
 import './util.dart';
 import 'package:bias_profile/Pages/RoomInProgressPage.dart';
 
-// Mixinとして実装することで、必要な画面で再利用可能にする
 mixin RoomStatusMonitor<T extends StatefulWidget> on State<T> {
   Stream<DocumentSnapshot>? _roomStream;
   bool _hasShownCancelMessage = false;
@@ -17,25 +16,26 @@ mixin RoomStatusMonitor<T extends StatefulWidget> on State<T> {
       if (!mounted) return;
 
       final data = snapshot.data() as Map<String, dynamic>?;
-      if (data != null &&
-          data['status'] == 'closed' &&
-          !_hasShownCancelMessage &&
-          !isCreator) {
-        _hasShownCancelMessage = true;
-        showConfirmationDialog(
+      if (data == null) return;
+
+      switch (data['status']) {
+        case 'closed' when !isCreator && !_hasShownCancelMessage:
+          _hasShownCancelMessage = true;
+          showConfirmationDialog(
+              context: context,
+              title: '部屋作成者が募集をキャンセルしました。',
+              onConfirm: () async {
+                Navigator.popUntil(context, ModalRoute.withName('/'));
+              });
+          break;
+
+        case _ when data['current_turn']['turn_count'] == 1:
+          showDialog(
             context: context,
-            title: '部屋作成者が募集をキャンセルしました。',
-            onConfirm: () async {
-              Navigator.popUntil(context, ModalRoute.withName('/'));
-            });
-      } else if (data != null && data['current_turn']['turn_count'] == 1) {
-        showDialog(
-          context: context,
-          barrierDismissible: false,
-          builder: (BuildContext context) =>
-              ProgressDialog(titleText: 'ターンを開始する準備をしています。'),
-        );
-        if (context.mounted) {
+            barrierDismissible: false,
+            builder: (BuildContext context) =>
+                ProgressDialog(titleText: 'ターンを開始する準備をしています。'),
+          );
           await Future.delayed(Duration(seconds: 3));
           Navigator.pushReplacement(
             context,
@@ -46,14 +46,13 @@ mixin RoomStatusMonitor<T extends StatefulWidget> on State<T> {
               ),
             ),
           );
-        }
+          break;
       }
     });
   }
 
   @override
   void dispose() {
-    // Stateが破棄されるときにStreamを解放
     _roomStream = null;
     super.dispose();
   }
