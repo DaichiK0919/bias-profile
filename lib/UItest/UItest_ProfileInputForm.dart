88e8af5fe5fe9f934e4a7c8e21c99cd29270c2c6
DaichiK0919 <new.ihadaacidkit@gmail.com>
2024-11-21 03:09:05 +0900

    プロフィール入力画面UI作成　画像取得処理実装（CORSエラー未解決）

---
+++ lib/UItest/UItest_ProfileInputForm.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:bias_profile/commons/constants.dart';
+
+class UitestProfileInputForm extends StatelessWidget {
+  const UitestProfileInputForm({super.key});
+  final double containerWidth = 500;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: containerWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(0, kMarginMedium, 0, kMarginMedium),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(kPaddingLarge),
+                  child: Column(
+                    children: [
+                      Text(
+                        'こちらの人物の見た目から勝手に想像して\n指定したプロフィールを入力してください',
+                      ),
+                      SizedBox(height: 16.0),
+                      Image(
+                          image: NetworkImage('https://picsum.photos/236/340')),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(
+                    kMarginLarge, kMarginMedium, kMarginLarge, kPaddingLarge),
+                child: Form(
+                  child: TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                      labelText: '（ここにお題が入る）',
+                      labelStyle: Theme.of(context).textTheme.labelMedium,
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('入力完了');
+                },
+                child: Text('入力完了'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/commons/constants.dart
+++ lib/commons/constants.dart
@@ -4,4 +4,5 @@ final double kPaddingLarge = 16.0;
 final double kFontSizeMedium = 14.0;
 final double kFontSizeLarge = 28.0;
 final double kMarginLarge = 16.0;
+final double kMarginMedium = 14.0;
 final int kProcessingDuration = 3;
