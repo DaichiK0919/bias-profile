99a9ff109a6d9c5c42ec415e8f0242f4bbfbc50e
DaichiK0919 <new.ihadaacidkit@gmail.com>
2025-02-11 00:54:11 +0900

    RoomInProgressForm: スクロールコンテナ不要のため削除

--- lib/components/RoomInProgressForm.dart
+++ lib/components/RoomInProgressForm.dart
@@ -68,137 +68,134 @@ class _RoomInProgressFormState extends State<RoomInProgressForm>
     return Center(
       child: Container(
         width: widget.containerWidth,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    margin: EdgeInsets.symmetric(vertical: kMarginLarge),
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    child: PlayerList(
-                      documentSnapshot: widget.streamAsMap,
-                      listTitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '得点表',
-                            style: Theme.of(context).textTheme.labelLarge,
-                          ),
-                        ],
-                      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(vertical: kMarginLarge),
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: PlayerList(
+                    documentSnapshot: widget.streamAsMap,
+                    listTitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '得点表',
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                    stream: widget.streamAsMap,
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return CircularProgressIndicator();
-                      }
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                  stream: widget.streamAsMap,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return CircularProgressIndicator();
+                    }
 
-                      final data = snapshot.data!.data();
-                      if (data == null) return Container();
+                    final data = snapshot.data!.data();
+                    if (data == null) return Container();
 
-                      final currentTurn =
-                          data['current_turn'] as Map<String, dynamic>;
-                      final parentPlayerId =
-                          currentTurn['parent_player_id'] as String?;
-                      if (parentPlayerId == null) return Container();
+                    final currentTurn =
+                        data['current_turn'] as Map<String, dynamic>;
+                    final parentPlayerId =
+                        currentTurn['parent_player_id'] as String?;
+                    if (parentPlayerId == null) return Container();
 
-                      return FutureBuilder<Map<String, dynamic>?>(
-                        future:
-                            findPlayerByUUID(widget.roomId, parentPlayerId!),
-                        builder: (context, playerSnapshot) {
-                          if (!playerSnapshot.hasData) {
-                            return CircularProgressIndicator();
-                          }
+                    return FutureBuilder<Map<String, dynamic>?>(
+                      future: findPlayerByUUID(widget.roomId, parentPlayerId!),
+                      builder: (context, playerSnapshot) {
+                        if (!playerSnapshot.hasData) {
+                          return CircularProgressIndicator();
+                        }
 
-                          final player = playerSnapshot.data;
-                          if (player == null) return Container();
+                        final player = playerSnapshot.data;
+                        if (player == null) return Container();
 
-                          return Container(
-                            width: double.infinity,
-                            margin: EdgeInsets.all(kMarginLarge),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'このターンの親は',
-                                  textAlign: TextAlign.center,
-                                  style: Theme.of(context).textTheme.bodyLarge,
-                                ),
-                                SizedBox(height: kMarginLarge),
-                                Text(
-                                  player['nickname'] as String,
-                                  textAlign: TextAlign.center,
-                                  style:
-                                      Theme.of(context).textTheme.headlineSmall,
-                                ),
-                              ],
-                            ),
-                          );
-                        },
+                        return Container(
+                          width: double.infinity,
+                          margin: EdgeInsets.all(kMarginLarge),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                'このターンの親は',
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                              SizedBox(height: kMarginLarge),
+                              Text(
+                                player['nickname'] as String,
+                                textAlign: TextAlign.center,
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    final data = await getRoomSnapshotAsMap(widget.roomId);
+                    final currentTurn =
+                        data['current_turn'] as Map<String, dynamic>;
+                    final isParent =
+                        currentTurn['parent_player_id'] == widget.playerId;
+
+                    if (!mounted) return;
+
+                    if (isParent) {
+                      // Parentの場合は全ての画像をプリキャッシュしてダイアログを表示
+                      showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) => ProgressDialog(
+                          titleText: '子のターンです。入力が完了するまでお待ちください。',
+                        ),
                       );
-                    },
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      final data = await getRoomSnapshotAsMap(widget.roomId);
-                      final currentTurn =
-                          data['current_turn'] as Map<String, dynamic>;
-                      final isParent =
-                          currentTurn['parent_player_id'] == widget.playerId;
-
-                      if (!mounted) return;
-
-                      if (isParent) {
-                        // Parentの場合は全ての画像をプリキャッシュしてダイアログを表示
-                        showDialog(
-                          context: context,
-                          barrierDismissible: false,
-                          builder: (BuildContext context) => ProgressDialog(
-                            titleText: '子のターンです。入力が完了するまでお待ちください。',
+                      _precacheAllImages().then((_) {
+                        if (!mounted) return;
+                        // プリキャッシュ完了後に何か処理が必要な場合はここに記述
+                      });
+                    } else {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProfileInputPage(
+                            roomId: widget.roomId,
+                            playerId: widget.playerId,
+                            stream: widget.streamAsMap,
+                            correctCardPath: widget.correctCardPath,
+                            otherCardPaths: widget.otherCardPaths,
+                            assignedProfileTheme: widget.assignedProfileTheme,
                           ),
-                        );
-                        _precacheAllImages().then((_) {
-                          if (!mounted) return;
-                          // プリキャッシュ完了後に何か処理が必要な場合はここに記述
-                        });
-                      } else {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ProfileInputPage(
-                              roomId: widget.roomId,
-                              playerId: widget.playerId,
-                              stream: widget.streamAsMap,
-                              correctCardPath: widget.correctCardPath,
-                              otherCardPaths: widget.otherCardPaths,
-                              assignedProfileTheme: widget.assignedProfileTheme,
-                            ),
-                          ),
-                        );
-                      }
-                    },
-                    child: Text('次に進む'),
-                  )
-                ],
-              ),
-            ],
-          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('次に進む'),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
